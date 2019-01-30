cc81e98cf7747bd83bf68a2a3c0d0d2e3827fdf8
andre <xplay568@gmail.com>
2019-01-30 09:14:41 -0600

    initial

---
+++ app/src/main/java/com/example/andres/laboratorio4/webViewer.kt
@@ -0,0 +1,22 @@
+package com.example.andres.laboratorio4
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_web_viewer.*
+
+class webViewer : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_viewer)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
